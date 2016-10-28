ae917599381dd1d6c59a20865ff2c523bceb6479
Ishan Subedi <ikushum@gmail.com>
2016-10-28 06:18:26 +0000

    created edit and update methods for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :photo, PhotoUploader
+  has_many :Posts
 end
 
-#i am here
